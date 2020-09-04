926e8dd75256bdb7a778ee60e505d1b9740edfa0
pawcio <pawelzarzycki89@gmail.com>
2020-09-04 15:48:10 +0200

    availability basic events setup

--- public/js/app.js
+++ public/js/app.js
@@ -1899,6 +1899,52 @@ module.exports = {
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/bookable/Availability.vue?vue&type=script&lang=js&":
+/*!********************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/bookable/Availability.vue?vue&type=script&lang=js& ***!
+  \********************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      from: null,
+      to: null
+    };
+  },
+  methods: {
+    check: function check() {
+      alert("Sprawdzam dostepnosc");
+    }
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/bookable/Bookable.vue?vue&type=script&lang=js&":
 /*!****************************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/bookable/Bookable.vue?vue&type=script&lang=js& ***!
@@ -38343,49 +38389,104 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
-}
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", [
-      _c(
-        "h6",
-        {
-          staticClass:
-            "text-secondary text-uppercase text-center font-weight-bold"
-        },
-        [_vm._v("Check availability")]
-      ),
+  return _c("div", [
+    _c(
+      "h6",
+      {
+        staticClass:
+          "text-secondary text-uppercase text-center font-weight-bold"
+      },
+      [_vm._v("Check availability")]
+    ),
+    _vm._v(" "),
+    _c("div", { staticClass: "form-row" }, [
+      _c("div", { staticClass: "form-group col-md-6" }, [
+        _c("label", { attrs: { for: "from" } }, [_vm._v("FROM")]),
+        _vm._v(" "),
+        _c("input", {
+          directives: [
+            {
+              name: "model",
+              rawName: "v-model",
+              value: _vm.from,
+              expression: "from"
+            }
+          ],
+          staticClass: "form-control form-control-sm",
+          attrs: { type: "date", id: "from", placeholder: "From date" },
+          domProps: { value: _vm.from },
+          on: {
+            keyup: function($event) {
+              if (
+                !$event.type.indexOf("key") &&
+                _vm._k($event.keyCode, "enter", 13, $event.key, "Enter")
+              ) {
+                return null
+              }
+              return _vm.check($event)
+            },
+            input: function($event) {
+              if ($event.target.composing) {
+                return
+              }
+              _vm.from = $event.target.value
+            }
+          }
+        })
+      ]),
       _vm._v(" "),
-      _c("div", { staticClass: "form-row" }, [
-        _c("div", { staticClass: "form-group col-md-6" }, [
-          _c("label", { attrs: { for: "from" } }, [_vm._v("FROM")]),
-          _vm._v(" "),
-          _c("input", {
-            staticClass: "form-control form-control-sm",
-            attrs: { type: "date", id: "from", placeholder: "From date" }
-          })
-        ]),
+      _c("div", { staticClass: "form-group col-md-6" }, [
+        _c("label", { attrs: { for: "to" } }, [_vm._v("TO")]),
         _vm._v(" "),
-        _c("div", { staticClass: "form-group col-md-6" }, [
-          _c("label", { attrs: { for: "to" } }, [_vm._v("TO")]),
-          _vm._v(" "),
-          _c("input", {
-            staticClass: "form-control form-control-sm",
-            attrs: { type: "date", placeholder: "To date" }
-          })
-        ]),
-        _vm._v(" "),
-        _c("button", { staticClass: "btn btn-secondary btn-block mr-1 ml-1" }, [
-          _vm._v("CHECK")
-        ])
-      ])
+        _c("input", {
+          directives: [
+            {
+              name: "model",
+              rawName: "v-model",
+              value: _vm.to,
+              expression: "to"
+            }
+          ],
+          staticClass: "form-control form-control-sm",
+          attrs: { type: "date", placeholder: "To date" },
+          domProps: { value: _vm.to },
+          on: {
+            keyup: function($event) {
+              if (
+                !$event.type.indexOf("key") &&
+                _vm._k($event.keyCode, "enter", 13, $event.key, "Enter")
+              ) {
+                return null
+              }
+              return _vm.check($event)
+            },
+            input: function($event) {
+              if ($event.target.composing) {
+                return
+              }
+              _vm.to = $event.target.value
+            }
+          }
+        })
+      ]),
+      _vm._v(" "),
+      _c(
+        "button",
+        {
+          staticClass: "btn btn-secondary btn-block mr-1 ml-1",
+          on: {
+            click: function($event) {
+              $event.preventDefault()
+              return _vm.check($event)
+            }
+          }
+        },
+        [_vm._v("CHECK")]
+      )
     ])
-  }
-]
+  ])
+}
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -53911,15 +54012,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Availability_vue_vue_type_template_id_65b9bf8c___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Availability.vue?vue&type=template&id=65b9bf8c& */ "./resources/js/components/bookable/Availability.vue?vue&type=template&id=65b9bf8c&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _Availability_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Availability.vue?vue&type=script&lang=js& */ "./resources/js/components/bookable/Availability.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _Availability_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _Availability_vue_vue_type_template_id_65b9bf8c___WEBPACK_IMPORTED_MODULE_0__["render"],
   _Availability_vue_vue_type_template_id_65b9bf8c___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
@@ -53933,6 +54036,20 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 if (false) { var api; }
 component.options.__file = "resources/js/components/bookable/Availability.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/bookable/Availability.vue?vue&type=script&lang=js&":
+/*!************************************************************************************!*\
+  !*** ./resources/js/components/bookable/Availability.vue?vue&type=script&lang=js& ***!
+  \************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Availability_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib??ref--4-0!../../../../node_modules/vue-loader/lib??vue-loader-options!./Availability.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/bookable/Availability.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Availability_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
--- resources/js/components/bookable/Availability.vue
+++ resources/js/components/bookable/Availability.vue
@@ -4,13 +4,33 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="from">FROM</label>
-                <input type="date" class="form-control form-control-sm" id="from" placeholder="From date">
+                <input type="date" class="form-control form-control-sm" id="from" placeholder="From date"
+                    v-model="from"
+                    v-on:keyup.enter="check">
             </div>
             <div class="form-group col-md-6">
                 <label for="to">TO</label>
-                <input type="date" class="form-control form-control-sm" placeholder="To date">
+                <input type="date" class="form-control form-control-sm" placeholder="To date"
+                    v-model="to"
+                    v-on:keyup.enter="check">
             </div>
-            <button class="btn btn-secondary btn-block mr-1 ml-1">CHECK</button>
+            <button class="btn btn-secondary btn-block mr-1 ml-1" @click.prevent="check">CHECK</button>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            from: null,
+            to: null
+        }
+    }, 
+    methods: {
+        check: function() {
+            alert("Sprawdzam dostepnosc")
+        }
+    }
+}
+</script>
