2ca11a205764420ca49b2e92d3e943270bb38bae
pawcio <pawelzarzycki89@gmail.com>
2020-10-11 23:03:08 +0200

    Address model with migrations and relations

--- public/js/app.js
+++ public/js/app.js
@@ -2028,6 +2028,59 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   computed: _objectSpread(_objectSpread({}, Object(vuex__WEBPACK_IMPORTED_MODULE_0__["mapState"])({
@@ -61418,10 +61471,8 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", [
-    _c("div", { staticClass: "row" }, [
-      _c("div", { staticClass: "col-md-7" }, [
-        _vm._v("\n            Checkout Form\n        ")
-      ]),
+    _c("div", { staticClass: "row shadow p-3 mb-5 rounded" }, [
+      _vm._m(0),
       _vm._v(" "),
       _c(
         "div",
@@ -61539,7 +61590,7 @@ var render = function() {
                 "pt-2 pb-2 d-flex justify-content-between border-top summary"
             },
             [
-              _vm._m(0),
+              _vm._m(1),
               _vm._v(" "),
               _c("span", [
                 _c("i", { staticClass: "fas fa-dollar-sign" }),
@@ -61554,6 +61605,110 @@ var render = function() {
   ])
 }
 var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "col-md-7" }, [
+      _vm._v("\n            Checkout Form\n            "),
+      _vm._v(" "),
+      _c("div", { staticClass: "row" }, [
+        _c("div", { staticClass: "col-md-6 form-group-row" }, [
+          _c("label", { attrs: { for: "first_name" } }, [_vm._v("First Name")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: {
+              type: "text",
+              id: "first_name",
+              placeholder: "First Name..."
+            }
+          })
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-md-6 form-group" }, [
+          _c("label", { attrs: { for: "last_name" } }, [_vm._v("Last Name")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: {
+              type: "text",
+              id: "last_name",
+              placeholder: "Last Name..."
+            }
+          })
+        ])
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "row" }, [
+        _c("div", { staticClass: "col-md-12 form-group" }, [
+          _c("label", { attrs: { for: "email" } }, [_vm._v("Email")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "email", id: "email", placeholder: "Email..." }
+          })
+        ])
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "row" }, [
+        _c("div", { staticClass: "col-md-6 form-group" }, [
+          _c("label", { attrs: { for: "city" } }, [_vm._v("City")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "text", id: "city", placeholder: "City..." }
+          })
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-md-6 form-group" }, [
+          _c("label", { attrs: { for: "street" } }, [_vm._v("Street")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "text", id: "street", placeholder: "Street..." }
+          })
+        ])
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "row" }, [
+        _c("div", { staticClass: "col-md-6 form-group" }, [
+          _c("label", { attrs: { for: "country" } }, [_vm._v("Country")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "text", id: "country", placeholder: "Country..." }
+          })
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-md-4 form-group" }, [
+          _c("label", { attrs: { for: "state" } }, [_vm._v("State")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "text", id: "state", placeholder: "State..." }
+          })
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-md-2 form-group" }, [
+          _c("label", { attrs: { for: "zip" } }, [_vm._v("Zip")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "text", id: "zip", placeholder: "Zip..." }
+          })
+        ])
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "row" }, [
+        _c("div", { staticClass: "col-md-12 form-group" }, [
+          _c("button", { staticClass: "btn btn-info btn-block" }, [
+            _vm._v("Book now")
+          ])
+        ])
+      ])
+    ])
+  },
   function() {
     var _vm = this
     var _h = _vm.$createElement
--- resources/js/components/basket/Basket.vue
+++ resources/js/components/basket/Basket.vue
@@ -1,8 +1,61 @@
 <template>
     <div>
-        <div class="row">
+        <div class="row shadow p-3 mb-5 rounded">
             <div class="col-md-7">
                 Checkout Form
+                <!-- first name and last name -->
+                <div class="row">
+                    <div class="col-md-6 form-group-row">
+                        <label for="first_name">First Name</label>
+                        <input type="text" class="form-control" id="first_name" placeholder="First Name...">
+                    </div>
+                    <div class="col-md-6 form-group">
+                        <label for="last_name">Last Name</label>
+                        <input type="text" class="form-control" id="last_name" placeholder="Last Name...">
+                    </div>
+                </div>
+
+                <!-- Email  -->
+                <div class="row">
+                    <div class="col-md-12 form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="Email...">
+                    </div>
+                </div>
+
+                <!-- Street and City -->
+                <div class="row">
+                    <div class="col-md-6 form-group">
+                        <label for="city">City</label>
+                        <input type="text" class="form-control" id="city" placeholder="City...">
+                    </div>
+                    <div class="col-md-6 form-group">
+                        <label for="street">Street</label>
+                        <input type="text" class="form-control" id="street" placeholder="Street...">
+                    </div>
+                </div>
+
+                <!-- Country State Zip -->
+                <div class="row">
+                    <div class="col-md-6 form-group">
+                        <label for="country">Country</label>
+                        <input type="text" class="form-control" id="country" placeholder="Country...">
+                    </div>
+                    <div class="col-md-4 form-group">
+                        <label for="state">State</label>
+                        <input type="text" class="form-control" id="state" placeholder="State...">
+                    </div>
+                    <div class="col-md-2 form-group">
+                        <label for="zip">Zip</label>
+                        <input type="text" class="form-control" id="zip" placeholder="Zip...">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12 form-group">
+                        <button class="btn btn-info btn-block">Book now</button>
+                    </div>    
+                </div>
             </div>
             <div class="col-md-5">
                 <div class="d-flex justify-content-between">
